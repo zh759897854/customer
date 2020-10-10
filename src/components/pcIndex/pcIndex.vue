47d84037c9016201b08dce7f72b6304e433249b5
759897854@qq.comm <759897854@qq.com>
2020-10-10 18:13:02 +0800

    2020-09-28

--- src/components/pcIndex/pcIndex.vue
+++ src/components/pcIndex/pcIndex.vue
@@ -1,6 +1,32 @@
 <style lang="less">
   #pc {
-
+    .banner-main {
+      position: relative;
+      min-height: 200px;
+      background-color: #000;
+      .page-nav {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        text-align: center;
+        z-index: 101;
+        .nav-title {
+          padding: 10px 15px;
+          color: #fff;
+          display: inline-block;
+          cursor: pointer;
+        }
+        .nav-title:hover {
+          background-color: #1f90e6;
+        }
+      }
+      .main-banner {
+        width: 100%;
+        opacity:0.6;
+        filter: alpha(opacity=60);
+      }
+    }
   }
 </style>
 
@@ -9,8 +35,9 @@
     <div class="page-banner">
       <div class="banner-main">
         <ul class="page-nav">
-          <li></li>
+          <li class="nav-title" v-for="(item, index) in nav">{{item}}</li>
         </ul>
+        <img class="main-banner" :src="banner_img1" alt="" />
       </div>
       <div class="banner-minor"></div>
     </div>
@@ -22,7 +49,16 @@
     name: 'pc',
     data() {
       return {
-
+        banner_img1: require('../../assets/back/back-banner1.jpg'),
+        nav: [
+          '首页',
+          '关于树人',
+          '学校介绍',
+          '大马优势',
+          '成功案例',
+          '留学指南',
+          '联系我们'
+        ]
       }
     },
     mounted() {
