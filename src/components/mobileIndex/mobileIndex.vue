bcad344a7fe85a2982f5528dc832b4c9173a93ac
759897854@qq.comm <759897854@qq.com>
2020-10-22 11:09:09 +0800

    default change

--- src/components/mobileIndex/mobileIndex.vue
+++ src/components/mobileIndex/mobileIndex.vue
@@ -1,10 +1,51 @@
-<style>
-
+<style lang="less">
+  .nave {
+    background-color: #545c64;
+    text-align: center;
+    position: fixed;
+    width: 100%;
+    .el-menu {
+      display: inline-block;
+      vertical-align: top;
+      border-bottom: none !important;
+      .shuren-logo {
+        background-image: url("../../assets/icon/icon-logo-shuren.png");
+        background-repeat: no-repeat;
+        background-size: 100%;
+        background-position: center;
+        margin: 0 30px !important;
+        background-color: #545c64;
+        cursor: default !important;
+      }
+      .shuren-logo:hover {
+        background-color: transparent !important;
+      }
+    }
+  }
 </style>
 
 <template>
   <div id="mobile">
-    持续开发中，敬请期待
+    <div class="nave">
+      <el-menu
+          :default-active="'1'"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#3786c8">
+        <el-menu-item class="shuren-logo"></el-menu-item>
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item index="2">关于树人</el-menu-item>
+        <el-menu-item index="3">学校介绍</el-menu-item>
+        <el-menu-item index="4">大马优势</el-menu-item>
+        <el-menu-item index="5">成功案例</el-menu-item>
+        <el-menu-item index="6">留学指南</el-menu-item>
+        <el-menu-item index="7">联系我们</el-menu-item>
+      </el-menu>
+    </div>
+    <div class="page-main"></div>
   </div>
 </template>
 
--- src/components/pcIndex/pcIndex.vue
+++ src/components/pcIndex/pcIndex.vue
@@ -1,5 +1,31 @@
 <style lang="less">
   #pc {
+    padding-top: 60px;
+    .nave {
+      background-color: #545c64;
+      text-align: center;
+      position: fixed;
+      top: 0;
+      width: 100%;
+      z-index: 10001;
+      .el-menu {
+        display: inline-block;
+        vertical-align: top;
+        border-bottom: none !important;
+        .shuren-logo {
+          background-image: url("../../assets/icon/icon-logo-shuren.png");
+          background-repeat: no-repeat;
+          background-size: 100%;
+          background-position: center;
+          margin: 0 30px !important;
+          background-color: #545c64;
+          cursor: default !important;
+        }
+        .shuren-logo:hover {
+          background-color: transparent !important;
+        }
+      }
+    }
 		h5 {
 			padding-top: 15px;
 			letter-spacing: 5px;
@@ -605,14 +631,33 @@
 
 <template>
   <div id="pc">
+    <div class="nave">
+      <el-menu
+          :default-active="'1'"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#3786c8">
+        <el-menu-item class="shuren-logo"></el-menu-item>
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item index="2">关于树人</el-menu-item>
+        <el-menu-item index="3">学校介绍</el-menu-item>
+        <el-menu-item index="4">大马优势</el-menu-item>
+        <el-menu-item index="5">成功案例</el-menu-item>
+        <el-menu-item index="6">留学指南</el-menu-item>
+        <el-menu-item index="7">联系我们</el-menu-item>
+      </el-menu>
+    </div>
     <div class="page-banner">
       <section class="banner-main">
-        <div class="shuren-logo">
-          <img :src="shuren_logo" alt="" />
-        </div>
-        <ul class="page-nav">
-          <li class="nav-title" :class="{'active' : navDistance === index}" v-for="(item, index) in nav" @click="toBlock(index)">{{item}}</li>
-        </ul>
+        <!--<div class="shuren-logo">-->
+          <!--<img :src="shuren_logo" alt="" />-->
+        <!--</div>-->
+        <!--<ul class="page-nav">-->
+          <!--<li class="nav-title" :class="{'active' : navDistance === index}" v-for="(item, index) in nav" @click="toBlock(index)">{{item}}</li>-->
+        <!--</ul>-->
         <img class="main-banner-back" :src="banner_img1" alt="" />
         <div class="text-container">
           <p class="study-abroad">专注马来西亚留学</p>
